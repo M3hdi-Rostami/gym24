7fbc066da036f424bb5b3fc75ddcaf1055f1de0f
M3hdi-Rostami <m3hdi.rostami@gmail.com>
2024-07-04 18:16:14 +0330

    Feat: replace mdi/react with mdi/font package for generate icons

--- src/components/Icon.jsx
+++
@@ -1,5 +0,0 @@
-const Icon = ({ name, className }) => {
-  return <i className={`mdi mdi-${name} ${className}`} />;
-};
-
-export default Icon;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/scss/index.scss";
-import "@mdi/font/css/materialdesignicons.min.css";
 
 import App from "./App";
 
