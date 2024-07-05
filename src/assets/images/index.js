d1bf7ac54133e566883ebb990ba6068a916ce723
M3hdi-Rostami <m3hdi.rostami@gmail.com>
2024-07-05 15:20:11 +0330

    Feat: Add slides images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -4,5 +4,20 @@ import lineIcon from "./icon_line.svg";
 import eqipmentIcon from "./icon_ eqipment.svg";
 import fitnessZonesIcon from "./icon_ fitness zones.svg";
 import timeIcon from "./icon_time.svg";
+import slide1 from "./slide1.svg";
+import slide2 from "./slide2.svg";
+import slide3 from "./slide3.svg";
+import slide4 from "./slide4.svg";
 
-export { logo, headerBg, lineIcon, eqipmentIcon, fitnessZonesIcon, timeIcon };
+export {
+  logo,
+  headerBg,
+  lineIcon,
+  eqipmentIcon,
+  fitnessZonesIcon,
+  timeIcon,
+  slide1,
+  slide2,
+  slide3,
+  slide4,
+};
